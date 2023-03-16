454c5593f39a2cd7fa49e37cd4597bb653452020
Chimes of Destruction <127018596+ChimesOfDestruction@users.noreply.github.com>
2023-03-16 18:59:34 +0000

    Add files via upload

---
+++ Random Icon/Form1.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.NumericUpDown4 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown6 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown8 = New System.Windows.Forms.NumericUpDown()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer5 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.HuraForm1 = New Icono_random.HuraForm()
+        Me.HuraCheckBox1 = New Icono_random.HuraCheckBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.HuraButton2 = New Icono_random.HuraButton()
+        Me.HuraButton1 = New Icono_random.HuraButton()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown8, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.HuraForm1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 32)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(344, 24)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "C:\iconorandom.ico"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"32x32", "48x48", "52x52", "64x64"})
+        Me.ComboBox1.Location = New System.Drawing.Point(72, 62)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(284, 25)
+        Me.ComboBox1.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 65)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(62, 17)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Icon Size:"
+        '
+        'NumericUpDown4
+        '
+        Me.NumericUpDown4.Location = New System.Drawing.Point(58, 93)
+        Me.NumericUpDown4.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.NumericUpDown4.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown4.Name = "NumericUpDown4"
+        Me.NumericUpDown4.Size = New System.Drawing.Size(70, 24)
+        Me.NumericUpDown4.TabIndex = 10
+        Me.NumericUpDown4.Value = New Decimal(New Integer() {255, 0, 0, 0})
+        '
+        'NumericUpDown6
+        '
+        Me.NumericUpDown6.Location = New System.Drawing.Point(58, 123)
+        Me.NumericUpDown6.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.NumericUpDown6.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown6.Name = "NumericUpDown6"
+        Me.NumericUpDown6.Size = New System.Drawing.Size(70, 24)
+        Me.NumericUpDown6.TabIndex = 13
+        Me.NumericUpDown6.Value = New Decimal(New Integer() {255, 0, 0, 0})
+        '
+        'NumericUpDown8
+        '
+        Me.NumericUpDown8.Location = New System.Drawing.Point(58, 156)
+        Me.NumericUpDown8.Maximum = New Decimal(New Integer() {255, 0, 0, 0})
+        Me.NumericUpDown8.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown8.Name = "NumericUpDown8"
+        Me.NumericUpDown8.Size = New System.Drawing.Size(70, 24)
+        Me.NumericUpDown8.TabIndex = 16
+        Me.NumericUpDown8.Value = New Decimal(New Integer() {255, 0, 0, 0})
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 69
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 25
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 132
+        '
+        'Timer4
+        '
+        Me.Timer4.Interval = 188
+        '
+        'Timer5
+        '
+        Me.Timer5.Interval = 500
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(10, 95)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 17)
+        Me.Label2.TabIndex = 21
+        Me.Label2.Text = "Red:"
+        '
+        'HuraForm1
+        '
+        Me.HuraForm1.AccentColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.HuraForm1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.HuraForm1.ColorScheme = Icono_random.HuraForm.ColorSchemes.Dark
+        Me.HuraForm1.Controls.Add(Me.HuraCheckBox1)
+        Me.HuraForm1.Controls.Add(Me.Label5)
+        Me.HuraForm1.Controls.Add(Me.Label4)
+        Me.HuraForm1.Controls.Add(Me.Label3)
+        Me.HuraForm1.Controls.Add(Me.HuraButton2)
+        Me.HuraForm1.Controls.Add(Me.HuraButton1)
+        Me.HuraForm1.Controls.Add(Me.Label2)
+        Me.HuraForm1.Controls.Add(Me.TextBox1)
+        Me.HuraForm1.Controls.Add(Me.NumericUpDown8)
+        Me.HuraForm1.Controls.Add(Me.ComboBox1)
+        Me.HuraForm1.Controls.Add(Me.NumericUpDown6)
+        Me.HuraForm1.Controls.Add(Me.Label1)
+        Me.HuraForm1.Controls.Add(Me.NumericUpDown4)
+        Me.HuraForm1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.HuraForm1.Font = New System.Drawing.Font("Segoe UI", 9.5!)
+        Me.HuraForm1.ForeColor = System.Drawing.Color.Gray
+        Me.HuraForm1.Location = New System.Drawing.Point(0, 0)
+        Me.HuraForm1.MaximumSize = New System.Drawing.Size(368, 213)
+        Me.HuraForm1.MinimumSize = New System.Drawing.Size(368, 213)
+        Me.HuraForm1.Name = "HuraForm1"
+        Me.HuraForm1.Size = New System.Drawing.Size(368, 213)
+        Me.HuraForm1.TabIndex = 23
+        Me.HuraForm1.Text = "Random Icon Generator Mod By ChimesOfDestruction"
+        '
+        'HuraCheckBox1
+        '
+        Me.HuraCheckBox1.BaseColour = System.Drawing.Color.White
+        Me.HuraCheckBox1.BorderColour = System.Drawing.Color.FromArgb(CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer), CType(CType(180, Byte), Integer))
+        Me.HuraCheckBox1.Checked = False
+        Me.HuraCheckBox1.CheckedColour = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.HuraCheckBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.HuraCheckBox1.FontColour = System.Drawing.Color.FromArgb(CType(CType(150, Byte), Integer), CType(CType(150, Byte), Integer), CType(CType(150, Byte), Integer))
+        Me.HuraCheckBox1.Location = New System.Drawing.Point(135, 94)
+        Me.HuraCheckBox1.Name = "HuraCheckBox1"
+        Me.HuraCheckBox1.Size = New System.Drawing.Size(221, 22)
+        Me.HuraCheckBox1.TabIndex = 28
+        Me.HuraCheckBox1.Text = "Randomize the RGB Values"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(9, 189)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(336, 15)
+        Me.Label5.TabIndex = 27
+        Me.Label5.Text = "Original: https://github.com/SadFud/Random-Icon-Generator"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(9, 158)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 17)
+        Me.Label4.TabIndex = 26
+        Me.Label4.Text = "Blue:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 125)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 17)
+        Me.Label3.TabIndex = 25
+        Me.Label3.Text = "Green:"
+        '
+        'HuraButton2
+        '
+        Me.HuraButton2.BackColor = System.Drawing.Color.Transparent
+        Me.HuraButton2.BaseColour = System.Drawing.Color.White
+        Me.HuraButton2.BorderColour = System.Drawing.Color.Gray
+        Me.HuraButton2.Font = New System.Drawing.Font("Segoe UI", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.HuraButton2.FontColour = System.Drawing.Color.FromArgb(CType(CType(150, Byte), Integer), CType(CType(150, Byte), Integer), CType(CType(150, Byte), Integer))
+        Me.HuraButton2.HoverColour = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.HuraButton2.Location = New System.Drawing.Point(134, 120)
+        Me.HuraButton2.Name = "HuraButton2"
+        Me.HuraButton2.PressedColour = System.Drawing.Color.FromArgb(CType(CType(245, Byte), Integer), CType(CType(245, Byte), Integer), CType(CType(245, Byte), Integer))
+        Me.HuraButton2.ProgressColour = System.Drawing.Color.Gray
+        Me.HuraButton2.Size = New System.Drawing.Size(222, 60)
+        Me.HuraButton2.TabIndex = 24
+        Me.HuraButton2.Text = "Generate Icon"
+        '
+        'HuraButton1
+        '
+        Me.HuraButton1.BackColor = System.Drawing.Color.Transparent
+        Me.HuraButton1.BaseColour = System.Drawing.Color.White
+        Me.HuraButton1.BorderColour = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(170, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.HuraButton1.FontColour = System.Drawing.Color.FromArgb(CType(CType(150, Byte), Integer), CType(CType(150, Byte), Integer), CType(CType(150, Byte), Integer))
+        Me.HuraButton1.HoverColour = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.HuraButton1.Location = New System.Drawing.Point(343, 3)
+        Me.HuraButton1.Name = "HuraButton1"
+        Me.HuraButton1.PressedColour = System.Drawing.Color.FromArgb(CType(CType(245, Byte), Integer), CType(CType(245, Byte), Integer), CType(CType(245, Byte), Integer))
+        Me.HuraButton1.ProgressColour = System.Drawing.Color.Red
+        Me.HuraButton1.Size = New System.Drawing.Size(23, 23)
+        Me.HuraButton1.TabIndex = 23
+        Me.HuraButton1.Text = "X"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(368, 213)
+        Me.Controls.Add(Me.HuraForm1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(368, 213)
+        Me.MinimumSize = New System.Drawing.Size(368, 213)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Icon Generator By Alpha"
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown8, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.HuraForm1.ResumeLayout(False)
+        Me.HuraForm1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents NumericUpDown4 As NumericUpDown
+    Friend WithEvents NumericUpDown6 As NumericUpDown
+    Friend WithEvents NumericUpDown8 As NumericUpDown
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents Timer4 As Timer
+    Friend WithEvents Timer5 As Timer
+    Friend WithEvents Label2 As Label
+    Friend WithEvents HuraForm1 As HuraForm
+    Friend WithEvents HuraButton1 As HuraButton
+    Friend WithEvents HuraButton2 As HuraButton
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents HuraCheckBox1 As HuraCheckBox
+End Class
---
+++ Random Icon/Form1.vb
@@ -0,0 +1,151 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Public Function GetRandom(ByVal Min As Integer, ByVal Max As Integer) As Integer
+        Static Generator As System.Random = New System.Random()
+        Return Generator.Next(Min, Max)
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = AppDomain.CurrentDomain.BaseDirectory & "\Icons\" & Path.GetRandomFileName & ".ico"
+        ComboBox1.SelectedItem = "48x48"
+        Dim YourPath = AppDomain.CurrentDomain.BaseDirectory & "\Icons\"
+
+        If Not System.IO.Directory.Exists(YourPath) Then
+            System.IO.Directory.CreateDirectory(YourPath)
+        End If
+
+
+
+    End Sub
+    Public Function generateicon() As String
+        Dim tamaño As String
+        If ComboBox1.Text = "32x32" Then
+            tamaño = "33"
+        End If
+        If ComboBox1.Text = "48x48" Then
+            tamaño = "49"
+        End If
+        If ComboBox1.Text = "52x52" Then
+            tamaño = "53"
+        End If
+        If ComboBox1.Text = "64x64" Then
+            tamaño = "64"
+        End If
+        Dim ancho As Integer = tamaño
+        Dim alto As Integer = tamaño
+        Dim bmp As New Bitmap(ancho, alto)
+        Dim rand As Random = New Random()
+        For y As Integer = 0 To alto - 1
+            For x As Integer = 0 To ancho - 1
+                '   Dim a As Integer = rand.Next(256)
+                Dim a As Integer = 255
+                Dim r As Integer = rand.Next(NumericUpDown4.Value)
+                Dim g As Integer = rand.Next(NumericUpDown6.Value)
+                Dim b As Integer = rand.Next(NumericUpDown8.Value)
+                bmp.SetPixel(x, y, Color.FromArgb(a, r, g, b))
+            Next
+        Next
+        Dim HIcon As IntPtr = bmp.GetHicon()
+        Dim nuevoicono As Icon = Icon.FromHandle(HIcon)
+
+        ' Dim rutafinal As String = Path.GetTempPath() + "iconorandom.ico"
+        Dim rutafinal As String = TextBox1.Text
+        Dim oFileStream As FileStream = New IO.FileStream(rutafinal, FileMode.CreateNew)
+        nuevoicono.Save(oFileStream)
+        oFileStream.Close()
+        Return rutafinal
+    End Function
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs)
+        Randomize()
+
+        Timer5.Start()
+        Timer1.Start()
+        Timer2.Start()
+        Timer3.Start()
+        Timer4.Start()
+
+
+
+
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        Randomize()
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        NumericUpDown4.Value = GetRandom(1, 256)
+        NumericUpDown6.Value = GetRandom(1, 256)
+        NumericUpDown8.Value = GetRandom(1, 256)
+        Randomize()
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        '  NumericUpDown6.Value = GetRandom(1, 256)
+        Randomize()
+    End Sub
+
+    Private Sub Timer4_Tick(sender As Object, e As EventArgs) Handles Timer4.Tick
+        '  NumericUpDown8.Value = GetRandom(1, 256)
+        Randomize()
+    End Sub
+
+    Private Sub Timer5_Tick(sender As Object, e As EventArgs) Handles Timer5.Tick
+        Timer1.Stop()
+        Timer2.Stop()
+        Timer3.Stop()
+        Timer4.Stop()
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub HuraButton1_Click(sender As Object, e As EventArgs) Handles HuraButton1.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub HuraButton2_Click(sender As Object, e As EventArgs) Handles HuraButton2.Click
+
+        Dim rutaicono As String = TextBox1.Text
+        Dim tamaño As String
+        If ComboBox1.Text = "32x32" Then
+            tamaño = "33"
+        End If
+        If ComboBox1.Text = "48x48" Then
+            tamaño = "49"
+        End If
+        If ComboBox1.Text = "52x52" Then
+            tamaño = "53"
+        End If
+        If ComboBox1.Text = "64x64" Then
+            tamaño = "64"
+        End If
+        If File.Exists(rutaicono) Then
+            File.Delete(rutaicono)
+            Call generateicon()
+
+        Else
+            Call generateicon()
+            TextBox1.Text = AppDomain.CurrentDomain.BaseDirectory & "\Icons\" & Path.GetRandomFileName & ".ico"
+        End If
+
+    End Sub
+
+    Private Sub HuraCheckBox1_CheckedChanged(sender As Object) Handles HuraCheckBox1.CheckedChanged
+        If HuraCheckBox1.Checked = True Then
+            Timer2.Start()
+        Else
+            Timer2.Stop()
+        End If
+    End Sub
+End Class
